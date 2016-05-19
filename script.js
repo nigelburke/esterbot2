8950e9c34a2195e129904b6caadc9aac855f5d02
nigelburke <nigelburke@gmail.com>
2016-05-20 00:01:19 +1000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}`))
+                .then(() => bot.say(`Great! nice to meet you ${name}`))
                 .then(() => 'speak');
         }
     },

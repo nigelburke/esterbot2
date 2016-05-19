fd519b6507b328e1d9bb72e452f2b90529bea928
nigelburke <nigelburke@gmail.com>
2016-05-19 15:48:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about your new job? Just say HELLO to get started.')
+            return bot.say('Hi. This is JBot. Send a message to get started.')
                 .then(() => 'speak');
         }
     },
@@ -29,7 +29,7 @@ module.exports = new Script({
                         return bot.setProp("silent", true);
                     case "DISCONNECT":
                         return bot.setProp("silent", false);
-                        default:
+                    default:
                         return Promise.resolve();
                 }
             }
@@ -38,13 +38,44 @@ module.exports = new Script({
                 return bot.getProp("silent");
             }
 
+
+/* getReply should allow for some variety in responses for received text messages that 
+do not have an entry in the scripts.json file. */
+            function getReply() {
+                var messages = [ "Sorry. I'm not configured with a response to your message. Text COMMANDS to see a few examples.",
+                                 "Hey, I didn't understand that. I suggest sending HELP OUT",
+                                 "Text me ABOUT to learn about the JBot project.",
+                                 "You're not sending messages to an Artifical Intelligence program. Try MORE to see some other COMMANDS",
+                                 "The program responds to COMMANDS only. You have to send a command that I understand. :)",
+                                 "The JBot is not a human. It is just a series of files on a computer. Text ABOUT to learn more.",
+                                 "Seriously, you are wayyyyy smarter than JBot. It just knows simple COMMANDS",
+                                 "Yo. I do not know what you are talking about. Send me a HELLO",
+                                 "There is a ton of information in JBot. You have to use COMMANDS to find it.",
+                                 "That's interesting. Hhhmmm... I never thought of that. Maybe try HELP OUT",
+                                 "Can you say that again?",
+                                 "Yeah... that happens from time to time. Try COMMANDS or ABOUT.",
+                                 "That is a ton of words you just wrote there... I really don't know. Try MORE",
+                                 "Right now, punctuation throws me off. Send text without it. Try HELP OUT",
+                                 "Try sending a command without punctuation.",
+                                 "I'm not programmed to ignore punctuation. So if you're sending something other than letters... I don't understand it."
+                                ];
+
+                var arrayIndex = Math.floor( Math.random() * messages.length );
+
+
+                return messages[arrayIndex];
+                
+            }
+
             function processMessage(isSilent) {
                 if (isSilent) {
                     return Promise.resolve("speak");
                 }
 
+/* remove the text in between the () after bot.say and place the function getReply() */
+
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I'm sorry, I didn't understand that. Try typing HELP`).then(() => 'speak');
+                    return bot.say( getReply() ).then( () => 'speak');
                 }
 
                 var response = scriptRules[upperText];
